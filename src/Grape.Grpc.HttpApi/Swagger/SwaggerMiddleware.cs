744644ed997ae15c99358a019aa56dda75cc63d2
Xuanye Wong <xuanye.wan@gmail.com>
2024-06-07 13:55:44 +0800

    update to net6.0

--- src/Grape.Grpc.HttpApi/Swagger/SwaggerMiddleware.cs
+++ src/Grape.Grpc.HttpApi/Swagger/SwaggerMiddleware.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +15,7 @@ namespace Grape.Grpc.HttpApi.Swagger
 
         public SwaggerMiddleware(
             SwaggerOptions options,
-            RequestDelegate next,            
+            RequestDelegate next,
             ILogger<SwaggerMiddleware> logger
             )
         {
@@ -38,17 +37,16 @@ namespace Grape.Grpc.HttpApi.Swagger
                 var swagger = swaggerProvider.GetSwaggerInfo();
                 await RespondWithSwaggerJson(httpContext.Response, swagger);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _logger.LogError(ex, ex.Message);
                 RespondWithNotFound(httpContext.Response);
             }
-
         }
 
         private bool RequestingSwaggerDocument(HttpRequest request)
         {
-            if(!string.Equals(request.Method, "get", StringComparison.OrdinalIgnoreCase))
+            if (!string.Equals(request.Method, "get", StringComparison.OrdinalIgnoreCase))
             {
                 return false;
             }
@@ -66,14 +64,9 @@ namespace Grape.Grpc.HttpApi.Swagger
             response.StatusCode = 200;
             response.ContentType = "application/json;charset=utf-8";
 
-            var settings = new Newtonsoft.Json.JsonSerializerSettings
-            {
-                DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Ignore
-            };
+            var swaggerJson = System.Text.Json.JsonSerializer.Serialize(swagger, new System.Text.Json.JsonSerializerOptions(System.Text.Json.JsonSerializerDefaults.Web));
 
-            var swaggerJson = Newtonsoft.Json.JsonConvert.SerializeObject(swagger, Newtonsoft.Json.Formatting.None, settings);
             await response.WriteAsync(swaggerJson, new UTF8Encoding(false));
         }
     }
-       
 }
